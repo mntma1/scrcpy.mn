de81f1d4677d83b1f6de4b3c3e20e16882968158
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-07-06 23:08:28 +0200

    Fix variable names and correct spelling in pcam.sh

--- pcam.sh
+++ pcam.sh
@@ -13,9 +13,9 @@ echo -e "$GNB
    b = Back Camera
    h = Horizontal(Cam) 
    v = Vertikal(Cam)
-   c = Screen Cupture
+   s = Screen Cupture
    d = Dateimanager(Startet den Filebrowser)
-   s = scrcpy (Cupture only)
+   c = scrcpy (Cupture only)
    m = scrcpy Manual Page
    x = Exit (Beendet das Script) 
 $CL"
@@ -25,7 +25,7 @@ echo;
 
 function orientation {
 while true; do
-    read -p "Die Orientierung des mobilPhone bitte (h/v/c/d/s/m/x): " Ausrichtung
+    read -p "Die Orientierung des mobilPhone bitte (f/b/h/v/c/d/s/m/x): " Ausrichtung
     case $Ausrichtung in
         [f]* ) scrcpy --video-source=camera  --camera-facing=front --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=270 -w &>/dev/null&;; # Front Cam
         
@@ -35,11 +35,11 @@ while true; do
         
         [v]* ) scrcpy --video-source=camera --camera-id=0 --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=90 -w&>/dev/null&;;
         
-        [c]* ) scrcpy --display-id=0 --new-display=800x600/160 --video-codec=av1 -x -w&>/dev/null&;;
+        [s]* ) scrcpy --display-id=0 --new-display=800x600/160 --video-codec=av1 -x -w&>/dev/null&;;
         
         [d]* ) scrcpy --display-id=0 --new-display=800x600/160 --video-codec=av1 --start-app=com.alphainventor.filemanager -x -w&>/dev/null&;;
  
-        [s]* ) scrcpy --display-id=0 --max-fps=60 --print-fps -w&>/dev/null&;;
+        [c]* ) scrcpy --display-id=0 --max-fps=60 --print-fps -w&>/dev/null&;;
         
         [m]* ) mate-terminal -e 'man scrcpy'&;;
         
@@ -49,13 +49,13 @@ while true; do
 
   Bitte antworte mit: 
  
-     f = Font Camara
-     b = Back Camera
+     f = Font Kamera
+     b = Back Kamera
      h = Horizontal
      v = Vertikal
-     c = Screen Cupture
+     s = Screen Cupture
      d = Dateimanager
-     s = scrcpy (Cupture only)
+     c = scrcpy (Cupture only)
      m = scrcpy Manual Page
      x = Exit (Beendet das Script) 
 
