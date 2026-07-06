c594ef2f998d6c709604e0e75edb4567151d4cf4
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-07-06 21:07:52 +0200

    add repo scrcpy.mn

---
+++ pcam.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# Colors
+source ~/bin/colors.env
+
+clear;
+echo -e "$GNB 
+
+ Bitte wähle mit: 
+   f = Font Camara
+   b = Back Camera
+   h = Horizontal(Cam) 
+   v = Vertikal(Cam)
+   c = Screen Cupture
+   d = Dateimanager(Startet den Filebrowser)
+   s = scrcpy (Cupture only)
+   m = scrcpy Manual Page
+   x = Exit (Beendet das Script) 
+$CL"
+
+echo -e "$OK $BLUB Viel Spaß beim Testen :)$CL"
+echo;
+
+function orientation {
+while true; do
+    read -p "Die Orientierung des mobilPhone bitte (h/v/c/d/s/m/x): " Ausrichtung
+    case $Ausrichtung in
+        [f]* ) scrcpy --video-source=camera  --camera-facing=front --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=270 -w &>/dev/null&;; # Front Cam
+        
+        [b]* ) scrcpy --video-source=camera  --camera-facing=back --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=90 -w &>/dev/null&;; # Back  Cam
+        
+        [h]* ) scrcpy --video-source=camera --camera-id=1 --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=0 -w &>/dev/null&;; 
+        
+        [v]* ) scrcpy --video-source=camera --camera-id=0 --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=90 -w&>/dev/null&;;
+        
+        [c]* ) scrcpy --display-id=0 --new-display=800x600/160 --video-codec=av1 -x -w&>/dev/null&;;
+        
+        [d]* ) scrcpy --display-id=0 --new-display=800x600/160 --video-codec=av1 --start-app=com.alphainventor.filemanager -x -w&>/dev/null&;;
+ 
+        [s]* ) scrcpy --display-id=0 --max-fps=60 --print-fps -w&>/dev/null&;;
+        
+        [m]* ) mate-terminal -e 'man scrcpy'&;;
+        
+        [x]* ) exit;;
+     
+        * ) clear; echo -e "$RDB
+
+  Bitte antworte mit: 
+ 
+     f = Font Camara
+     b = Back Camera
+     h = Horizontal
+     v = Vertikal
+     c = Screen Cupture
+     d = Dateimanager
+     s = scrcpy (Cupture only)
+     m = scrcpy Manual Page
+     x = Exit (Beendet das Script) 
+
+$CL";;
+    esac
+done
+}
+orientation
+exit 0
