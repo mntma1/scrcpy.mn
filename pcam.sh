1750b791551602325d2be07ce3f8c1334f7aeda3
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-07-09 04:55:16 +0200

    Refactor pcam.sh to use variable for program name

--- pcam.sh
+++ pcam.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+PROG=scrcpy
 # Colors
-source ~/bin/colors.env
+source ~/usr/local/bin/colors.env
 
 clear;
 echo -e "$GNB 
@@ -21,21 +22,21 @@ echo;
 
 function orientation {
 while true; do
-    read -p "Die Orientierung des mobilPhone bitte (h/v/c/d/s/m/x): " Ausrichtung
+    read -p "Bitte wählen (f/bh/v/c/d/s/m/x): " Ausrichtung
     case $Ausrichtung in
-        [f]* ) scrcpy --video-source=camera  --camera-facing=front --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=270 -w &>/dev/null&;; # Front Cam
+        [f]* ) $PROG --video-source=camera  --camera-facing=front --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=270 -w &>/dev/null&;; # Front Cam
         
-        [b]* ) scrcpy --video-source=camera  --camera-facing=back --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=90 -w &>/dev/null&;; # Back  Cam
+        [b]* ) $PROG --video-source=camera  --camera-facing=back --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=90 -w &>/dev/null&;; # Back  Cam
         
-        [h]* ) scrcpy --video-source=camera --camera-id=1 --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=0 -w &>/dev/null&;; 
+        [h]* ) $PROG --video-source=camera --camera-id=1 --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=0 -w &>/dev/null&;; 
         
-        [v]* ) scrcpy --video-source=camera --camera-id=0 --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=90 -w&>/dev/null&;;
+        [v]* ) $PROG --video-source=camera --camera-id=0 --camera-fps=30 --camera-size=1920x1080 --no-audio --orientation=90 -w&>/dev/null&;;
         
-        [c]* ) scrcpy --display-id=0 --new-display=800x600/160 --video-codec=av1 -x -w&>/dev/null&;;
+        [c]* ) $PROG --display-id=0 --new-display=800x600/160 --video-codec=av1 -x -w&>/dev/null&;;
         
-        [d]* ) scrcpy --display-id=0 --new-display=800x600/160 --video-codec=av1 --start-app=com.alphainventor.filemanager -x -w&>/dev/null&;;
+        [d]* ) $PROG --display-id=0 --new-display=800x600/160 --video-codec=av1 --start-app=com.alphainventor.filemanager -x -w&>/dev/null&;;
  
-        [s]* ) scrcpy --display-id=0 --max-fps=60 --print-fps -w&>/dev/null&;;
+        [s]* ) $PROG --display-id=0 --max-fps=60 --print-fps -w&>/dev/null&;;
         
         [m]* ) mate-terminal -e 'man scrcpy'&;;
         
