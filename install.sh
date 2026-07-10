02050dcd632754318895b2304138801e5d0d7a01
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-07-10 18:41:55 +0200

    Fix symbolic link path for scrcpy installation

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo cp -v pcam.sh colors.env /usr/local/bin;
 wget https://github.com/Genymobile/scrcpy/releases/download/v4.0/scrcpy-linux-x86_64-v4.0.tar.gz;
 tar xvzf scrcpy-linux-x86_64-v4.0.tar.gz;
 sudo cp -av scrcpy-linux-x86_64-v4.0 /usr/local;
-sudo ln -s /usr/local/scrcpy-v4.0/scrcpy /usr/local/bin/scrcpy;
+sudo ln -s /usr/local/scrcpy-linux-x86_64-v4.0/scrcpy /usr/local/bin/scrcpy;
 cd ~/;
 /usr/local/bin/pcam.sh;
 exit 0
