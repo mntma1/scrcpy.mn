bd1e4c7bbeab406786fa201cf8acc7bd13e3e819
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-07-20 22:00:54 +0200

    Update install script to copy additional files

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Date: 10.07.26 at 02:45 am
 
 sudo apt install adb fastboot;
-sudo cp -fv pcam-yad.sh pcam.sh colors.env /usr/local/bin;
+sudo cp -fv pcam-yad1.sh pcam-yad-long.sh webcam-yad.sh pcam.sh colors.env /usr/local/bin;
 wget https://github.com/Genymobile/scrcpy/releases/download/v4.0/scrcpy-linux-x86_64-v4.0.tar.gz;
 tar xvzf scrcpy-linux-x86_64-v4.0.tar.gz;
 sudo cp -afv scrcpy-linux-x86_64-v4.0 /usr/local;
