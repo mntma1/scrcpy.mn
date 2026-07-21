ceb813603b7a077a4610f838dfac6a64dc50aafc
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-07-21 17:33:53 +0200

    Path: "/usr/local/scrcpy-linux-x86_64-v4.0/" war falsch :-(

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt install adb fastboot yad -y;
 sudo cp -fv pcam-yad1.sh pcam-yad-long.sh webcam-yad.sh pcam.sh colors.env /usr/local/bin;
 wget https://github.com/Genymobile/scrcpy/releases/download/v4.0/scrcpy-linux-x86_64-v4.0.tar.gz;
 tar xvzf scrcpy-linux-x86_64-v4.0.tar.gz;
-sudo cp -afv scrcpy-linux-x86_64-v4.0 /usr/local;
+sudo cp -afv scrcpy-linux-x86_64-v4.0/ /usr/local/
 sudo ln -sf /usr/local/scrcpy-linux-x86_64-v4.0/scrcpy /usr/local/bin/scrcpy;
 cd ~/;
 /usr/local/bin/pcam-yad1.sh;
