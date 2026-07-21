879bcc9f7ebd779697eed48898e74bf43936f758
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-07-21 16:44:57 +0200

    Add 'yad' to package installation in install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # Created by Manfred
 # Date: 10.07.26 at 02:45 am
 
-sudo apt install adb fastboot;
+sudo apt install adb fastboot yad -y;
 sudo cp -fv pcam-yad1.sh pcam-yad-long.sh webcam-yad.sh pcam.sh colors.env /usr/local/bin;
 wget https://github.com/Genymobile/scrcpy/releases/download/v4.0/scrcpy-linux-x86_64-v4.0.tar.gz;
 tar xvzf scrcpy-linux-x86_64-v4.0.tar.gz;
 sudo cp -afv scrcpy-linux-x86_64-v4.0 /usr/local;
 sudo ln -sf /usr/local/scrcpy-linux-x86_64-v4.0/scrcpy /usr/local/bin/scrcpy;
 cd ~/;
-/usr/local/bin/pcam.sh;
+/usr/local/bin/pcam-yad1.sh;
 exit 0
