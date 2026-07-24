46a9e002b7aea0d2c83334aeebf449c95292c060
Manfred N <114916491+mntma1@users.noreply.github.com>
2026-07-24 14:19:02 +0200

    Enhance install.sh with completion messages and links
    Updated installation script with additional information and improved messages.

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # Created by Manfred
 # Date: 10.07.26 at 02:45 am
 source ./colors.env
+
 sudo apt install adb fastboot yad -y;
 sudo cp -fv pcam-yad1.sh pcam-yad-long.sh webcam-yad.sh pcam.sh colors.env /usr/local/bin;
 cd /tmp
@@ -9,14 +10,36 @@ wget https://github.com/Genymobile/scrcpy/releases/download/v4.0/scrcpy-linux-x8
 tar xvzf scrcpy-linux-x86_64-v4.0.tar.gz;
 sudo cp -afv scrcpy-linux-x86_64-v4.0/ /usr/local/;
 sudo ln -sf /usr/local/scrcpy-linux-x86_64-v4.0/scrcpy /usr/local/bin/scrcpy;
-rm -rfv /tmp/scrcpy-linux-x86_64-v4.0*
+rm -rfv /tmp/scrcpy-linux-x86_64-v4.0*;
 cd ~/;
 /usr/local/bin/pcam-yad1.sh;
+sleep 2;
+clear;
 echo -e "$CYNB
 
-      Installation abgeschlossen
+  Die Installation ist  abgeschlossen :-)
 
-         Gruß Manne Maus
+           Gruß Manne Maus
+
+
+     Starten der GUI: 
+   
+        pcam-yad1.sh
+       
+    oder der Terminal Version:
+     
+        pcam.sh
+$CL"
+
+echo -e "$GNB  
+  
+  
+ Weitere Dokumentationen findet ihr in meinem Git Repo
+_____________________________________________________________
+   
+ https://github.com/mntma1/scrcpy.mn/blob/main/README.md
+
+ https://github.com/mntma1/scrcpy.mn/blob/main/scrcpy.md
 
 $CL"
 exit 0
